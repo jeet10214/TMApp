6651c7ba77fdce92d04e13ba2a74f3db9bd976ee
Jeet Kapadia <jeetkapadia@Jeets-MacBook-Air.local>
2020-12-12 21:37:16 +0530

    Removed Storyboard

---
+++ TMApp/TMApp/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  TMApp
+//
+//  Created by Jeet Kapadia on 12/12/20.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ TMApp/TMApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMApp
+//
+//  Created by Jeet Kapadia on 12/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
