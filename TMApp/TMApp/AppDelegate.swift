7afa50cd41a04b8b0d2ec07b2fb09d56074058a4
Jeet Kapadia <jeetkapadia@Jeets-MacBook-Air.local>
2020-12-12 22:26:21 +0530

    Added network layer

--- TMApp/TMApp/AppDelegate.swift
+++ TMApp/TMApp/AppDelegate.swift
@@ -12,8 +12,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        setRoot()
         return true
+    }
+    
+    private func setRoot() {
+        window = UIWindow()
+        let initialView = ViewController()
+        let navigationController = UINavigationController(rootViewController: initialView)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
     }
 }
 
